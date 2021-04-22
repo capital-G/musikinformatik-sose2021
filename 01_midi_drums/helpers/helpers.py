76b73059fc07e62fee592cac90d3fac0fea259f1
Dennis Scheiba <dennis.scheiba@gmx.de>
2021-04-22 20:55:36 +0200

    use note seq instead of music21

--- 01_midi_drums/helpers/helpers.py
+++ 01_midi_drums/helpers/helpers.py
@@ -1,29 +1,72 @@
-from io import StringIO 
-import sys
+import os
+from typing import Dict
+import json
 
-from music21 import stream
+import numpy as np
+from mido import KeySignatureError
+import pretty_midi as pm
+import note_seq
 
-class Capturing(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
+
+def default(obj):
+    if type(obj).__module__ == np.__name__:
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return obj.item()
+    raise TypeError('Unknown type:', type(obj))
+
+
+def parse_midi_file_async(
+    midi_file_path: str,
+    proto_save_path: str = '../datasets/lmd/proto/',
+    json_save_path: str = '../datasets/lmd/json/',
+    save_json: bool = False,
+) -> Dict:
+    pm.pretty_midi.MAX_TICK = 1e7
+    r = {
+        'midi_path': midi_file_path,
+        'midi_error': True,
+    }
+    midi_name = os.path.splitext(midi_file_path.split(os.sep)[-1])[0]
+    proto_path = os.path.join(proto_save_path, f'{midi_name}.protobuf')
+    json_path = os.path.join(json_save_path, f'{midi_name}.json')
     
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
-
-def stream_string(stream: stream.Stream, n_lines: int = 10) -> str:
-    """    
-    a small hack so we can capture the output of
-    music21 and shorten it so our notebook does not
-    get spammed.
-
-    uses a context manager from
-    https://stackoverflow.com/a/16571630/347577
-    """
-    with Capturing() as output:
-        stream.show('text')
-
-    return '\n'.join(output[0:n_lines])
+    try:
+        stream = pm.PrettyMIDI(midi_file_path)
+        r['midi_error'] = False
+    except (OSError, ValueError, IndexError, KeySignatureError, EOFError, ZeroDivisionError):
+        return r
+    
+    try:
+        # r['beat_start'] = stream.estimate_beat_start() # omitted b/c adds 200ms to parsing
+        r['estimate_tempo'] = stream.estimate_tempo()
+        r['tempi_sec'], r['tempi'] = stream.get_tempo_changes() 
+        r['end_time'] = stream.get_end_time()
+        r['drums'] = any([i.is_drum for i in stream.instruments])
+        r['resolution'] = stream.resolution
+        r['instrument_names'] = [i.name.strip() for i in stream.instruments]
+        r['num_time_signature_changes'] = len(stream.time_signature_changes)
+    except ValueError as e:
+        # ValueError: Can't estimate beat start when there are no notes.
+        # ValueError: Can't provide a global tempo estimate when there are fewer than two notes.
+        print(f"Could not parse MIDI file {midi_file_path}: {e}")
+    
+    try:
+        notes = note_seq.midi_to_note_sequence(stream)
+    except Exception as e:
+        print(f'Could not convert {midi_name} to note sequence')
+        return r
+    with open(proto_path, 'wb') as f:
+        f.write(notes.SerializeToString())
+    r['proto_path'] = proto_path
+    
+    if save_json:
+        with open(json_path, 'w') as f:
+            json.dump(r, f, default=default)
+    
+    # free memory
+    stream = None
+    notes = None
+    
+    return r
